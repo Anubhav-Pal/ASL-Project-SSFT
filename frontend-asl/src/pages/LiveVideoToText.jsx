44da505a746dfb187b89c6daa4ecef32e133e501
Anubhav <anubhavpal15june@gmail.com>
2023-06-21 13:42:27 +0530

    Pages Added

---
+++ frontend-asl/src/pages/LiveVideoToText.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const LiveVideoToText = () => {
+  return (
+    <div>LiveVideoToText</div>
+  )
+}
+
+export default LiveVideoToText
---
+++ frontend-asl/src/pages/TextToVideo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TextToVideo = () => {
+  return (
+    <div>TextToVideo</div>
+  )
+}
+
+export default TextToVideo
