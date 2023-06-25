d38d4b30ed8bbe5672b922ebe775f09dc221d5b2
Vanshika Jain <106269077+vanshika8750@users.noreply.github.com>
2023-06-25 18:26:23 +0530

    member section

--- frontend-asl/src/components/Footer.jsx
+++ frontend-asl/src/components/Footer.jsx
@@ -2,7 +2,43 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div>
+
+      <div >
+        <div>ASLife.</div>
+        <div>A new way to make the payments easy,reliable and secure.</div>
+      </div>
+
+      <div className='useful links'>
+    <div>Useful Links</div>
+    <ul>
+      <li>Content</li>
+      <li>How it Works</li>
+      <li>Create</li>
+      <li>Explore</li>
+      <li>terms & Services</li>
+    </ul>
+      </div>
+
+      <div className='community'>
+    <div>Community</div>
+    <ul>
+      <li>Help Center</li>
+      <li>Partners</li>
+      <li>Suggestions</li>
+      <li>Blog</li>
+      <li>Newsletters</li>
+    </ul>
+      </div>
+
+      <div className='partner'>
+    <div>Partner</div>
+    <ul>
+      <li>Our Partners</li>
+      <li>Become a Partner</li>
+    </ul>
+      </div>
+    </div>
   )
 }
 
