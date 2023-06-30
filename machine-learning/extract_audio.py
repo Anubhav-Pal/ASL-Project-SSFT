93ee637d7bfc1ed41dcaeeaaf517d634fb04dc6e
Vaani <vaanipathariya@gmail.com>
2023-06-30 11:25:16 +0530

    feat : extract_audio

--- machine-learning/extract_audio.py
+++ machine-learning/extract_audio.py
@@ -1,5 +1,5 @@
 import moviepy.editor
 
-video=moviepy.editor.VideoFileClip('machine-learning/rain1.mp4')
+video=moviepy.editor.VideoFileClip('machine-learning/test.mp4')
 audio = video.audio
 audio.write_audiofile('rain1_audio.mp3')
