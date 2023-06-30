15d660514f348af4790017f3682f31569db23523
Vanshika Jain <106269077+vanshika8750@users.noreply.github.com>
2023-06-30 13:42:37 +0530

    backend added

--- frontend-asl/src/App.jsx
+++ frontend-asl/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Emergency, Feedback, Landingpage, LiveVideoToText, TextToVideo, VideoToText } from './pages'
-import Login from './pages/Login'
+// import Login from './pages/Login'
 import Signup from './pages/Signup'
 
 const App = () => (
@@ -13,7 +13,7 @@ const App = () => (
       <Route path='/livevideototext' exact element={ <LiveVideoToText/>} />
       <Route path='/videototext' exact element={ <VideoToText/>} />
       <Route path='/texttovideo' exact element={ <TextToVideo/>} />
-      <Route path='/login' exact element={ <Login/>} />
+      {/* // <Route path='/login' exact element={ <Login/>} /> */}
       <Route path='/signup' exact element={<Signup/>}></Route>
     </Routes>
   </BrowserRouter>
--- frontend-asl/src/pages/Signup.jsx
+++ frontend-asl/src/pages/Signup.jsx
@@ -1,21 +1,53 @@
 import React, { useState } from "react";
+import axios from "axios";
 const Signup=()=>{
-    const [username,setUsername]=useState('')
+    const [email,setEmail]=useState('')
+    const [first,setFirst]=useState('')
+    const [last,setLast]=useState('')
     const [password,setPassword]=useState('')
-    const changeUsername=(event)=>{
-        setUsername(event.target.value)
+    const changeEmail=(event)=>{
+        setEmail(event.target.value)
     }
     const changePassword=(event)=>{
         setPassword(event.target.value)
     }
-    const Submit_=()=>{
-        console.log(username);
-        console.log(password);
+    const changeFirst=(event)=>{
+        setFirst(event.target.value)
+    }
+    const changeLast=(event)=>{
+        setLast(event.target.value)
+    }
+    const Submit_=(event)=>{
+        event.preventDefault()
+        const newUser = {
+            first_name: first,
+            last_name: last,
+            email: email,
+            password: password
+        }
+
+        register(newUser).then(res => {
+            console.log("done")
+        })
+    }
+    const register = newUser => {
+        return axios
+            .post("https://localhost:5000/users/register", {
+                first_name: newUser.first_name,
+                last_name: newUser.last_name,
+                email: newUser.email,
+                password: newUser.password
+            })
+            .then(response => {
+                console.log("Registered")
+            })
     }
     return(
         <div>
-            <input type="text" placeholder="Username" onChange={changeUsername}></input>
+            <input type="text" placeholder="email" onChange={changeEmail}></input>
             <input type="password" placeholder="Password" onChange={changePassword}></input>
+            <input type="text" placeholder="First Name" onChange={changeFirst}></input>
+            <input type="text" placeholder="Last Name" onChange={changeLast}></input>
             <button onClick={Submit_}>Submit</button>
         </div>
     )
